e06f3b4fe33df5e5eb7e7e02d1afe43a012799c4
Subhrajyoti <subha.ionic3@gmail.com>
2020-07-05 22:29:12 +0530

    added latter buttons

--- Project_7_8_9_Combine/ViewController.swift
+++ Project_7_8_9_Combine/ViewController.swift
@@ -18,8 +18,14 @@ class ViewController: UIViewController {
     
     var displayLabel: UILabel!
     var hintLabel: UILabel!
+    var lettersButtons = [UIButton]()
+    let lettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
     override func loadView() {
+        
+        let screenSize: CGRect = UIScreen.main.bounds
+        print(screenSize.width)
+        
         view = UIView()
         view.backgroundColor = .white
         
@@ -27,6 +33,7 @@ class ViewController: UIViewController {
         displayLabel.translatesAutoresizingMaskIntoConstraints = false
         displayLabel.text = ""
         displayLabel.font = UIFont.systemFont(ofSize: 40, weight: .bold)
+        displayLabel.backgroundColor = .lightGray
         view.addSubview(displayLabel)
         
         hintLabel = UILabel()
@@ -35,16 +42,50 @@ class ViewController: UIViewController {
         hintLabel.font = UIFont.systemFont(ofSize: 24)
         view.addSubview(hintLabel)
         
+        let buttonsView = UIView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        buttonsView.backgroundColor = .green
+        view.addSubview(buttonsView)
+        
         
         NSLayoutConstraint.activate([
-            displayLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             displayLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //            displayLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
             
             hintLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            hintLabel.topAnchor.constraint(equalTo: displayLabel.bottomAnchor, constant: 40)
+            hintLabel.topAnchor.constraint(equalTo: displayLabel.bottomAnchor, constant: 40),
+            
+            buttonsView.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor),
+            buttonsView.heightAnchor.constraint(equalToConstant: 180),
+            buttonsView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonsView.topAnchor.constraint(equalTo: hintLabel.bottomAnchor, constant: 40),
+            buttonsView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -20)
         ])
+        
+        let letterWidth = 53
+        let letterHeight = 40
+        
+        for row in 0...3 {
+            for col in 0...6 {
+                let letterButton = UIButton(type: .system)
+                letterButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+                letterButton.setTitle(" ", for: .normal)
+                letterButton.layer.borderWidth = 1
+                letterButton.layer.borderColor = UIColor(ciColor: .red).cgColor
+                
+                let frame = CGRect(x: col * letterWidth, y: row * letterHeight, width: letterWidth, height: letterHeight)
+                
+                letterButton.frame = frame
+                letterButton.isHidden = true
+                letterButton.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+                
+                buttonsView.addSubview(letterButton)
+                
+                lettersButtons.append(letterButton)
+            }
+        }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,7 +97,13 @@ class ViewController: UIViewController {
         if let levelFileUrl = Bundle.main.url(forResource: "level", withExtension: "txt") {
             if let levelContent = try? String(contentsOf: levelFileUrl) {
                 var lines = levelContent.components(separatedBy: "\n")
+                lines = lines.dropLast()
                 lines.shuffle()
+                
+                for i in 0..<lettersArray.count {
+                    lettersButtons[i].setTitle(lettersArray[i], for: .normal)
+                    lettersButtons[i].isHidden = false
+                }
                 
                 dataSet = lines
                 loadWordToGuess()
@@ -92,6 +139,10 @@ class ViewController: UIViewController {
         print(displayWord)
     }
     
+    @objc func letterTapped(_ sender: UIButton) {
+        print(sender.titleLabel?.text)
+    }
+    
     func displayModifiedString() {
         //
     }
@@ -103,7 +154,7 @@ class ViewController: UIViewController {
     func reload(action: UIAlertAction) {
         self.loadData()
     }
-
-
+    
+    
 }
 
